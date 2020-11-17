e2c477c66e1b970abfc60c6e749e0b81ba00e0a0
ridhonovembri@gmail.com <ridhonovembri@gmail.com>
2020-11-17 14:32:38 +0700

    added

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const app = express();
+const employeeController = require('./controllers/employeeController');
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./assets'));
 
 app.listen(8000);
 
 app.get('/', (req, res) => {
-    res.send('index');
+    res.render('index');
 })
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
